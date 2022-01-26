f029003fdbabf1c2c9fa2f30d3d47a7ddb95dd97
C Davis <cdavis@joegalaxy.com>
2022-01-26 14:29:26 -0600

    installed eslint & setup prettier, plus implemented a SafeAreaView

--- App.js
+++ App.js
@@ -1,15 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
+
+//const isAndroid = Platform.OS === 'android';
+console.log(StatusBar.currentHeight);
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Our react native blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={{ padding: 16, backgroundColor: 'green' }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
+  search: {
+    flex: 0.05,
+    backgroundColor: 'green',
+    padding: 10,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: 'blue',
+    padding: 10,
+  },
 });
