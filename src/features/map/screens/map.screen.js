92418645381e37337e69311ff6de3ef62e6ae962
C Davis <cdavis@joegalaxy.com>
2022-02-08 18:13:50 -0600

    implemented isMap for favourites image rendering

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -56,7 +56,7 @@ export const MapScreen = ({ navigation }) => {
                   })
                 }
               >
-                <MapCallout restaurant={restaurant} />
+                <MapCallout isMap restaurant={restaurant} />
               </MapView.Callout>
             </MapView.Marker>
           );
