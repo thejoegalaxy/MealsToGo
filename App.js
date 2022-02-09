c54d09641021ce798dd250068cb754ea006fc474
C Davis <cdavis@joegalaxy.com>
2022-02-08 20:33:38 -0600

    implemented firebase authentication

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/native';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 import {
   useFonts as useOswald,
@@ -15,7 +24,42 @@ import { RestaurantsContextProvider } from './src/services/restaurants/restauran
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCIN28Am9fVmGqoyhSWi1VlIG-iIflBGIA',
+  authDomain: 'mealstogo-c557b.firebaseapp.com',
+  projectId: 'mealstogo-c557b',
+  storageBucket: 'mealstogo-c557b.appspot.com',
+  messagingSenderId: '971645305284',
+  appId: '1:971645305284:web:99f2ac90d60a877a74a491',
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const auth = getAuth(app);
+//console.log(auth);
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(auth, 'cdavis@joegalaxy.com', 'test123')
+        .then((user) => {
+          //console.log(user);
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e.code, e.message);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -27,6 +71,8 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
 
   return (
     <>
