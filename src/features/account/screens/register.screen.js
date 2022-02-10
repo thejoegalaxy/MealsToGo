ff4546b3c558eddf6dfc6c49c9accf1c7119fc35
C Davis <cdavis@joegalaxy.com>
2022-02-09 20:32:09 -0600

    implemented an Activity Indicator on Registration & Login

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -1,4 +1,7 @@
 import React, { useState, useContext } from 'react';
+
+import { ActivityIndicator, Colors } from 'react-native-paper';
+
 import {
   AccountBackground,
   AccountCover,
@@ -16,8 +19,7 @@ export const RegisterScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatedPassword, setRepeatedPassword] = useState('');
-  const { onRegister, error } = useContext(AuthenticationContext);
-  //console.log(error);
+  const { onRegister, isLoading, error } = useContext(AuthenticationContext);
   return (
     <AccountBackground>
       <AccountCover />
@@ -53,17 +55,21 @@ export const RegisterScreen = ({ navigation }) => {
         </Spacer>
         {error && (
           <ErrorContainer size="large">
-            <Text variant="error">{error}</Text>
+            <Text variant="error">{error.message}</Text>
           </ErrorContainer>
         )}
         <Spacer size="large">
-          <AuthButton
-            icon="email"
-            mode="contained"
-            onPress={() => onRegister(email, password, repeatedPassword)}
-          >
-            Register
-          </AuthButton>
+          {!isLoading ? (
+            <AuthButton
+              icon="email"
+              mode="contained"
+              onPress={() => onRegister(email, password, repeatedPassword)}
+            >
+              Register
+            </AuthButton>
+          ) : (
+            <ActivityIndicator animating={true} color={Colors.blue300} />
+          )}
         </Spacer>
       </AccountContainer>
       <Spacer size="large">
